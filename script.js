c6241702904a170da465f0319599204cd02fecfa
Krish Chowta <134074335+krishxchowta@users.noreply.github.com>
2024-03-18 02:34:04 +0530

    FIRST

---
+++ script.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+}
+
+function openInstagramVideo() {
+    window.location.href = 'https://www.instagram.com/p/VIDEO_ID/';
+}
